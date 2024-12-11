67970435ad7de87ba80153cfd7656bde4e3be0e6
Rob Wood <rob@rnwood.co.uk>
2024-12-11 15:33:18 +0000

    Chore: Add E2E tests to protect against regressions

--- tests/Common.ps1
+++ tests/Common.ps1
@@ -2,19 +2,22 @@ BeforeAll {
 
     if ($env:TESTMODULEPATH) {
         $source = $env:TESTMODULEPATH
-    } else {
+    }
+    else {
         $source = "$PSScriptRoot/../Rnwood.Dataverse.Data.PowerShell/bin/Debug/netstandard2.0/"
     }
 
-     $tempmodulefolder = "${env:TEMP}/$([Guid]::NewGuid())"
+    $tempmodulefolder = "${env:TEMP}/$([Guid]::NewGuid())"
     new-item -ItemType Directory $tempmodulefolder
     copy-item -Recurse $source $tempmodulefolder/Rnwood.Dataverse.Data.PowerShell
     $env:PSModulePath = $tempmodulefolder;
+    $env:ChildProcessPSModulePath = $tempmodulefolder
      
     $metadata = $null;
 
     function getMockConnection() {
-        if (-not $metadata){
+        if (-not $metadata) {
+            Import-Module Rnwood.Dataverse.Data.PowerShell
             Add-Type -AssemblyName "System.Runtime.Serialization"
 
             # Define the DataContractSerializer
--- tests/Module.Tests.ps1
+++ tests/Module.Tests.ps1
@@ -1,28 +1,70 @@
-. $PSScriptRoot/Common.ps1
-
 Describe "Module" {
+    . $PSScriptRoot/Common.ps1
+
+    It "Given the module is installed, it is listed as available" {
+        Get-Module -ListAvailable Rnwood.Dataverse.Data.PowerShell | Should -HaveCount 1
+    }
+
     It "Given the module was not already loaded, it can be loaded successfully" {
-        Get-Module Rnwood.Dataverse.Data.PowerShell | Should -HaveCount 0
+        pwsh -noninteractive -noprofile -command {
+            $env:PSModulePath = $env:ChildProcessPSModulePath
+            if ([appdomain]::CurrentDomain.GetAssemblies() | Where-Object{ $_.GetName().Name -eq "Microsoft.Xrm.Sdk"}) {
+                throw "Expected assembly to not be already loaded"
+            }
 
-        Import-Module Rnwood.Dataverse.Data.PowerShell
+            if (Get-Module Rnwood.Dataverse.Data.PowerShell) {
+                throw "Expected module to not be loaded"
+            }
 
-        Get-Module Rnwood.Dataverse.Data.PowerShell | Should -HaveCount 1
+            Import-Module Rnwood.Dataverse.Data.PowerShell
+
+            if (-not (Get-Module Rnwood.Dataverse.Data.PowerShell)) {
+                throw "Expected module to be loaded"
+            }
+
+            if (-not ([appdomain]::CurrentDomain.GetAssemblies() | Where-Object{ $_.GetName().Name -eq "Microsoft.Xrm.Sdk"})) {
+                throw "Expected assembly to be loaded"
+            }
+        }
+
+        if ($LASTEXITCODE -ne 0) {
+            throw "Failed"
+        }
     }
 
     It "Given SDK assemblies are already loaded, it can be loaded successfully" {
 
-        Get-Module Rnwood.Dataverse.Data.PowerShell | Should -HaveCount 0
+        pwsh -noninteractive -noprofile -command {
+            $env:PSModulePath = $env:ChildProcessPSModulePath
+            if ([appdomain]::CurrentDomain.GetAssemblies() | Where-Object{ $_.GetName().Name -eq "Microsoft.Xrm.Sdk"}) {
+                throw "Expected assembly to not be already loaded"
+            }
 
-        $modulefolder = (Get-Module -ListAvailable Rnwood.Dataverse.Data.PowerShell).ModuleBase
+            if (-not (Get-Module Rnwood.Dataverse.Data.PowerShell)) {
+                throw "Expected module to not be loaded"
+            }
 
-        if ($PSVersionTable.PSEdition -eq "Core") {
-            add-type -AssemblyName $modulefolder/net6.0/Microsoft.Xrm.Sdk.dll
-        } else {
-            add-type -AssemblyName $modulefolder/net462/Microsoft.Xrm.Sdk.dll
+            $modulefolder = (Get-Module -ListAvailable Rnwood.Dataverse.Data.PowerShell).ModuleBase
+
+            if ($PSVersionTable.PSEdition -eq "Core") {
+                add-type -AssemblyName $modulefolder/net6.0/Microsoft.Xrm.Sdk.dll
+            } else {
+                add-type -AssemblyName $modulefolder/net462/Microsoft.Xrm.Sdk.dll
+            }
+
+            Import-Module Rnwood.Dataverse.Data.PowerShell
+
+            if (-not (Get-Module Rnwood.Dataverse.Data.PowerShell)) {
+                throw "Expected module to be loaded"
+            }
+
+            if (-not ([appdomain]::CurrentDomain.GetAssemblies() | Where-Object{ $_.GetName().Name -eq "Microsoft.Xrm.Sdk"})) {
+                throw "Expected assembly to be loaded"
+            }
         }
 
-        Import-Module Rnwood.Dataverse.Data.PowerShell
-
-        Get-Module Rnwood.Dataverse.Data.PowerShell | Should -HaveCount 1
+        if ($LASTEXITCODE -ne 0) {
+            throw "Failed"
+        }
     }
 }
