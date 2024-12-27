6b90028925bb6e76dfabfe63471ec45946624663
Rob Wood <rob@rnwood.co.uk>
2024-12-27 15:49:57 +0000

    Fix temp path for Linux.

--- tests/Common.ps1
+++ tests/Common.ps1
@@ -7,7 +7,7 @@ BeforeAll {
         $source = "$PSScriptRoot/../Rnwood.Dataverse.Data.PowerShell/bin/Debug/netstandard2.0/"
     }
 
-    $tempmodulefolder = "${env:TEMP}/$([Guid]::NewGuid())"
+    $tempmodulefolder = "$([IO.Path]::GetTempPath())/$([Guid]::NewGuid())"
     new-item -ItemType Directory $tempmodulefolder
     copy-item -Recurse $source $tempmodulefolder/Rnwood.Dataverse.Data.PowerShell
     $env:PSModulePath = $tempmodulefolder;
