9e4e9e4303b87a28f68c5fef65242e6893450bc7
Rob Wood <rob@rnwood.co.uk>
2025-08-03 09:17:03 +0100

    Fixup tests

--- tests/Module.Tests.ps1
+++ tests/Module.Tests.ps1
@@ -47,9 +47,9 @@ Describe "Module" {
             $modulefolder = (Get-Module -ListAvailable Rnwood.Dataverse.Data.PowerShell).ModuleBase
 
             if ($PSVersionTable.PSEdition -eq "Core") {
-                add-type -AssemblyName $modulefolder/net6.0/Microsoft.Xrm.Sdk.dll
+                add-type -AssemblyName $modulefolder/cmdlets/net6.0/Microsoft.Xrm.Sdk.dll
             } else {
-                add-type -AssemblyName $modulefolder/net462/Microsoft.Xrm.Sdk.dll
+                add-type -AssemblyName $modulefolder/cmdlets/net462/Microsoft.Xrm.Sdk.dll
             }
 
             Import-Module Rnwood.Dataverse.Data.PowerShell
