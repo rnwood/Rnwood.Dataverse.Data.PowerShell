f9fe4018adf8fcf30611f2e3e204279716784f9f
smtp4dev-automation <noreply@github.com>
2025-10-03 19:50:25 +0100

    Add metadata for more tables

--- tests/updatemetadata.ps1
+++ tests/updatemetadata.ps1
@@ -4,7 +4,7 @@ Add-Type -AssemblyName "System.Runtime.Serialization"
 # Define the DataContractSerializer
 $serializer = New-Object System.Runtime.Serialization.DataContractSerializer([Microsoft.Xrm.Sdk.Metadata.EntityMetadata])
 
-foreach($entityname in "contact") {
+foreach($entityname in "contact", "account", "systemuser", "workflow") {
 $em  = (invoke-dataverserequest -connection $c RetrieveEntity @{"LogicalName"=$entityname; "EntityFilters"=[Microsoft.Xrm.Sdk.Metadata.EntityFilters]::All; "MetadataId"=[Guid]::Empty; "RetrieveAsIfPublished"=$false}).Results["EntityMetadata"]
 
 $outputStream = [IO.File]::OpenWrite("$(get-location)/${entityname}.xml")
