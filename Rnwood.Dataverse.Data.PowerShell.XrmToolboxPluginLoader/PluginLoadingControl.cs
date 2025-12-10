066d04c0aa37e7501df6a2f57e67e6d33a047ca6
smtp4dev-automation <noreply@github.com>
2025-12-10 12:31:38 +0000

    fix(xtb): assembly load error when installed from store

--- Rnwood.Dataverse.Data.PowerShell.XrmToolboxPluginLoader/PluginLoadingControl.cs
+++ Rnwood.Dataverse.Data.PowerShell.XrmToolboxPluginLoader/PluginLoadingControl.cs
@@ -33,7 +33,6 @@ namespace Rnwood.Dataverse.Data.PowerShell.XrmToolboxPluginLoader
 
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PluginLoadingControl));
             this.mainPanel = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.statusLabel = new System.Windows.Forms.Label();
@@ -56,7 +55,7 @@ namespace Rnwood.Dataverse.Data.PowerShell.XrmToolboxPluginLoader
             // pictureBox1
             // 
             this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image = global::Rnwood.Dataverse.Data.PowerShell.XrmToolboxPluginLoader.Properties.Resources.logo;
             this.pictureBox1.Location = new System.Drawing.Point(12, 5);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(397, 103);
