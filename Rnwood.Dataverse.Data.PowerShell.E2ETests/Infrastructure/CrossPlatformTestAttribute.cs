f73b0b33c0b96f0f08c39ed89e7ef80f0b9088b7
Rob Wood <rob@rnwood.co.uk>
2026-02-11 07:47:22 +0000

    Fix

--- Rnwood.Dataverse.Data.PowerShell.E2ETests/Infrastructure/CrossPlatformTestAttribute.cs
+++ Rnwood.Dataverse.Data.PowerShell.E2ETests/Infrastructure/CrossPlatformTestAttribute.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
+using Xunit.Sdk;
 
 namespace Rnwood.Dataverse.Data.PowerShell.E2ETests.Infrastructure
 {
@@ -7,11 +9,17 @@ namespace Rnwood.Dataverse.Data.PowerShell.E2ETests.Infrastructure
     /// Marks E2E tests that should run on all platforms (Windows, Linux, macOS).
     /// Tests without this attribute will only run on Windows in CI.
     /// </summary>
+    [TraitDiscoverer("Rnwood.Dataverse.Data.PowerShell.E2ETests.Infrastructure.CrossPlatformTestDiscoverer", "Rnwood.Dataverse.Data.PowerShell.E2ETests")]
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false)]
-    public class CrossPlatformTestAttribute : TraitAttribute
+    public class CrossPlatformTestAttribute : Attribute, ITraitAttribute
     {
-        public CrossPlatformTestAttribute() : base("Category", "CrossPlatform")
+    }
+
+    public class CrossPlatformTestDiscoverer : ITraitDiscoverer
+    {
+        public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
         {
+            yield return new KeyValuePair<string, string>("Category", "CrossPlatform");
         }
     }
 }
